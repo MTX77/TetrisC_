67bcea66c5cb5dcd530df1a94f650a1f8481cd62
MTX <96997652+MTX77@users.noreply.github.com>
2024-04-25 12:34:36 +0200

    Update send-to-github.sh

--- send-to-github.sh
+++ send-to-github.sh
@@ -4,12 +4,9 @@ GITHUB_TOKEN="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"
 REPO_OWNER="MTX77"
 REPO_NAME="TetrisC_"
 BRANCH="master"
-FILE_PATH="./artifacts"
+FILE_PATH="./log/*"
 
 API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/artifacts"
-
-chown -R jenkins:jenkins ./artifacts
-chmod -R 775 ./artifacts
 
 curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
      -H "Content-Type: application/json" \
