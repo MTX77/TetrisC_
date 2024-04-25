3dfcf1ddc44a0bf5946901ebf1dd5f8caf43dbc3
MTX <96997652+MTX77@users.noreply.github.com>
2024-04-25 12:10:54 +0200

    Create send-to-github.sh

---
+++ send-to-github.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GITHUB_TOKEN="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"
+REPO_OWNER="MTX77"
+REPO_NAME="TetrisC_"
+BRANCH="master"
+FILE_PATH="./artifacts"
+
+API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/artifacts"
+
+curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
+     -H "Content-Type: application/json" \
+     -d "{\"message\": \"Dodano APK\", \"content\": \"$(base64 -w 0 ${FILE_PATH})\"}" \
+     ${API_URL}
